1e67825925412c13dffcea6c8db90c36db4c0504
unknown <msgill8146878895@gmail.com>
2021-12-11 16:41:58 +0530

    voiceandcallsearch

--- googlewithvoicerec/scripts/index.js
+++ googlewithvoicerec/scripts/index.js
@@ -10,7 +10,6 @@ const recognition = new SpeechRecognition();
 
 recognition.onstart = function () {
     action.innerHTML = "<small>listening, please speak......</small>";
-    window.speechSynthesis.speak("You can speak now");
     console.log("You can speak now!!");
 };
 
@@ -20,7 +19,6 @@ recognition.onresult = function (event) {
     console.log('confidence:', confidence)
     console.log(text);
     searchFormInput.value = text;
-
     read(text);
 };
 
@@ -42,7 +40,51 @@ function read(text) {
         window.location.href = `https://www.youtube.com/results?search_query=${text}`;
     else if (text.includes("owner"))
         speech.text = "My owner is Mehtab singh gill";
+    else if (text.includes("weather")) {
+        text= text.split(" ")[0]
+        getWeather(text)
+    }
 
     window.speechSynthesis.speak(speech);
     searchFormInput.value = speech.text;
 }
+
+document.getElementById("city").addEventListener("click", checkweather);
+
+function checkweather(){
+    console.log("hi")
+    document.getElementById("city").innerHTML ="Checking... <br> Please wait"
+    window.navigator.geolocation
+    .getCurrentPosition(successfulLookup, console.log);
+}
+
+
+    async function successfulLookup(position){
+        const {
+            latitude,
+            longitude
+        } = position.coords;
+       await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=f731490488974beaad4350748cbe4a5e`)
+            .then(response => response.json())
+            .then((data) => {
+                getWeather(data.results[0].components.state_district);
+              });
+     }
+    function showWeather(city, data) {
+        document.getElementById("city").innerHTML = `Temperature in ${city} City is ${data}° C`
+        
+    } 
+
+    async function getWeather(city) {
+        var res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2943c82a02fef99d328247102d3e473f`
+        );
+        let data = await res.json();
+        var temperature = Math.round(data.main.temp - 273.15);
+        console.log(temperature);
+        showWeather(city, temperature);
+      }
+    
+    
+
+               
